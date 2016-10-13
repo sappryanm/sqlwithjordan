58d530e4cb13d28f5a9681401e1c6af771a27894
Casey Borders <casey@techelevator.com>
2016-10-13 11:32:32 -0400

    Adding pair exercises

---
+++ m2-w1d3-joins/joins-exercises.sql
@@ -0,0 +1,32 @@
+-- Write queries to return the following:
+-- The following queries utilize the "world" database.
+
+-- 1. The city name, country name, and city population of all cities in Europe with population greater than 1 million
+--    Expected rows: 36
+
+-- 2. The city name, country name, and city population of all cities in countries where French is an official language and the city population is greater than 1 million
+--    Expected rows: 2
+
+-- 3. The name of the countries and continents where the language Javanese is spoken
+--    Expected rows: 1
+
+-- 4. The names of all of the countries in Africa that speak French as an official language
+--    Expected rows: 5
+
+-- 5. The average city population of cities in Europe
+--    Expected: 287,684
+
+-- 6. The average city population of cities in Asia
+--    Expected: 395,019
+
+-- 7. The number of cities in countries where English is an official language
+--    Expected: 523
+
+-- 8. The average population of cities in countries where the official language is English
+--    Expected: 285,809
+
+-- 9. The names of all of the continents and the population of the continent’s largest city
+--    Expected rows: 6, largest population for North America: 8,591,309
+
+-- 10. The names of all of the cities in South America that have a population of more than 1 million people and the official language of each city’s country
+--     Expected rows: 29
